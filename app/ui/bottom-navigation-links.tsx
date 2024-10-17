e2cb05658241e7832f49f922617b4f60d05fc6e6
lukasbicus <ing.lukas.bicus@gmail.com>
2024-10-17 16:10:29 +0200

    Make navigation interactive.

--- app/ui/bottom-navigation-links.tsx
+++ app/ui/bottom-navigation-links.tsx
@@ -29,13 +29,13 @@ export default function BottomNavigationLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+            className={clsx(
               {
-                'bg-sky-100 text-blue-600': pathname === link.href
+                'active': pathname === link.href
               })}
           >
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+            <LinkIcon className="w-6"/>
+            <span className="btm-nav-label">{link.name}</span>
           </Link>
         );
       })}
