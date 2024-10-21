4c9a9d114210dbed3adc5532d9968d2fa5f5f367
lukasbicus <ing.lukas.bicus@gmail.com>
2024-10-21 09:10:30 +0200

    Add an test script.

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
         backgroundImage: 'url(hero.jpg)',
       }}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-overlay bg-opacity-60"
+
+      > </div>
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Tilda</h1>
