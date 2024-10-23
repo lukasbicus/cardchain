94b69fd4a0b096d3cb556d33a3df20968ea21157
lukasbicus <ing.lukas.bicus@gmail.com>
2024-10-23 14:44:20 +0200

    Disable PWA in dev mode.

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const withSerwist = withSerwistInit({
   swSrc: 'app/sw.ts',
   swDest: 'public/sw.js',
   reloadOnOnline: true,
+  disable: process.env.NODE_ENV === 'development', // disable PWA in the development environment
 });
 
 export default withSerwist(nextConfig);
