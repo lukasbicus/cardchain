7a5c705d7f95daa5577485e34adb9de46a8cef36
lukasbicus <ing.lukas.bicus@gmail.com>
2024-10-24 13:34:51 +0200

    Add some icon buttons.

--- app/create-card/page.tsx
+++ app/create-card/page.tsx
@@ -3,28 +3,38 @@ import { DropdownField } from '@/app/ui/dropdown-field';
 import PageTemplate from '@/app/ui/page-template';
 import { TextAreaField } from '@/app/ui/text-area-field';
 import { TextField } from '@/app/ui/text-field';
+import { IconCamera, IconPalette } from '@tabler/icons-react';
 
 export default function Page() {
   return (
-    <PageTemplate header={<>secondary header</>}>
-      <div>Scan card page</div>
-      <form className="px-4 py-6">
-        <TextField label="Card number" />
+    <PageTemplate header={<div>secondary header</div>}>
+      <form className="px-4 py-6 w-full min-h-full">
+        <div className="flex gap-8">
+          <TextField label="Card number" />
+          <button className="btn btn-primary btn-square mt-9">
+            <IconCamera className="w-6 h-6" />
+          </button>
+        </div>
         <TextField label="Card name" />
         <TextAreaField label="Note" />
-        <DropdownField
-          label="Color"
-          value={Colors.Khaki}
-          options={Object.entries(Colors).map(([name, hex]) => ({
-            label: (
-              <div className="flex gap-2 items-center">
-                <div className="w-4 h-4" style={{ backgroundColor: hex }} />
-                <span>{name}</span>
-              </div>
-            ),
-            value: hex,
-          }))}
-        />
+        <div className="flex gap-8">
+          <DropdownField
+            label="Color"
+            value={Colors.Khaki}
+            options={Object.entries(Colors).map(([name, hex]) => ({
+              label: (
+                <div className="flex gap-2 items-center">
+                  <div className="w-4 h-4" style={{ backgroundColor: hex }} />
+                  <span>{name}</span>
+                </div>
+              ),
+              value: hex,
+            }))}
+          />
+          <button className="btn btn-primary btn-square mt-9">
+            <IconPalette className="w-6 h-6" />
+          </button>
+        </div>
         <DropdownField
           label="Category"
           value={CardIcon.Airlines}
@@ -37,6 +47,9 @@ export default function Page() {
             value: key,
           }))}
         />
+        {/*<button className="absolute bottom-6 btn btn-primary w-full">*/}
+        {/*  Submit*/}
+        {/*</button>*/}
       </form>
     </PageTemplate>
   );
