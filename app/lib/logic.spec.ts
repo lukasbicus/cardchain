5229fe0bda89a7829518fd4e2823854b4c3a1cdd
lukasbicus <ing.lukas.bicus@gmail.com>
2024-10-28 08:37:26 +0100

    Remove logic.

--- app/lib/logic.spec.ts
+++
@@ -1,8 +0,0 @@
-import { add } from './logic';
-import { describe, it, expect } from '@jest/globals';
-
-describe('logic', () => {
-  it('should add 2 numbers', () => {
-    expect(add(1, 2)).toBe(3);
-  });
-});
