7030ab6913692151b082b3aa2a69c87d96308d1c
lukasbicus <ing.lukas.bicus@gmail.com>
2024-11-11 11:04:47 +0100

    Update app images.

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,8 +37,8 @@ export default function RootLayout({
         <link
           rel="icon"
           type="image/png"
-          href="/favicon-48x48.png"
-          sizes="48x48"
+          href="/favicon-96x96.png"
+          sizes="96x96"
         />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
