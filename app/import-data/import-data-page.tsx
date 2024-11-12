3c312be52eddd3bcd2e574e317bdba70cab816c8
lukasbicus <ing.lukas.bicus@gmail.com>
2024-11-12 10:31:51 +0100

    Show a dialog on successful import.

--- app/import-data/import-data-page.tsx
+++ app/import-data/import-data-page.tsx
@@ -2,8 +2,12 @@
 
 import { getUniqParsedCards, parseCards } from '@/app/import-data/utils';
 import useAppState from '@/app/lib/app-state/app-state';
+import { Routes } from '@/app/lib/shared';
+import { ConfirmDialog } from '@/app/ui/confirm-dialog';
 import { FileImportField, getFileText } from '@/app/ui/file-import-field';
 import clsx from 'clsx';
+import { useRouter } from 'next/navigation';
+import { useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 enum ImportFormNames {
@@ -21,6 +25,8 @@ export function ImportDataPage() {
     formState: { errors, isValid },
   } = useForm<ImportForm>();
   const [state, dispatch] = useAppState();
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  const [countOfImportedCards, setCountOfImportedCards] = useState(0);
   const processFileImport = async (data: ImportForm) => {
     try {
       const text = await getFileText(data[ImportFormNames.FileList][0]);
@@ -38,35 +44,53 @@ export function ImportDataPage() {
       });
       // display count of added records in dialog
       // add options to navigate to my cards and to import more data
+      setCountOfImportedCards(uniqParsedCards.length);
+      dialogRef.current?.showModal();
     } catch (e) {
       console.log('error', e, JSON.stringify(e));
     }
   };
+  const router = useRouter();
   return (
-    <form
-      className="px-4 py-6 w-full h-full"
-      onSubmit={handleSubmit(processFileImport)}
-    >
-      <div className="px-4 py-6 bg-base-300">
-        <FileImportField
-          label="File to be imported"
-          name={ImportFormNames.FileList}
-          required
-          register={register}
-          errors={errors}
-        />
-      </div>
-      <footer className="btm-nav btm-nav-md text-base-content px-4">
-        <button
-          className={clsx('btn btn-primary w-full', {
-            disabled: !isValid,
-          })}
-          type="submit"
-          disabled={!isValid}
-        >
-          Import data
-        </button>
-      </footer>
-    </form>
+    <>
+      <form
+        className="px-4 py-6 w-full h-full"
+        onSubmit={handleSubmit(processFileImport)}
+      >
+        <div className="px-4 py-6 bg-base-300">
+          <FileImportField
+            label="File to be imported"
+            name={ImportFormNames.FileList}
+            required
+            register={register}
+            errors={errors}
+          />
+        </div>
+        <footer className="btm-nav btm-nav-md text-base-content px-4">
+          <button
+            className={clsx('btn btn-primary w-full', {
+              disabled: !isValid,
+            })}
+            type="submit"
+            disabled={!isValid}
+          >
+            Import data
+          </button>
+        </footer>
+      </form>
+      <ConfirmDialog
+        ref={dialogRef}
+        title="Import successful"
+        body={`Count of successfully imported cards: ${countOfImportedCards}. Lets check them in my cards section. Click close, if you want to import more cards.`}
+        confirmButtonLabel="Go to my cards"
+        cancelButtonLabel="Close"
+        onConfirmButtonClick={() => {
+          router.replace(Routes.MyCards);
+        }}
+        onCancelButtonClick={() => {
+          dialogRef.current?.close();
+        }}
+      />
+    </>
   );
 }
