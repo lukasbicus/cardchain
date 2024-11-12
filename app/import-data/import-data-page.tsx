ecd7592b26cf659a56d8b84b2c23f66406055016
lukasbicus <ing.lukas.bicus@gmail.com>
2024-11-12 10:07:39 +0100

    Log unique cards

--- app/import-data/import-data-page.tsx
+++ app/import-data/import-data-page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { getUniqParsedCards, parseCards } from '@/app/import-data/utils';
+import useAppState from '@/app/lib/app-state/app-state';
 import { FileImportField, getFileText } from '@/app/ui/file-import-field';
 import clsx from 'clsx';
 import { useForm } from 'react-hook-form';
@@ -18,14 +20,18 @@ export function ImportDataPage() {
     handleSubmit,
     formState: { errors, isValid },
   } = useForm<ImportForm>();
+  const [state, dispatch] = useAppState();
   const processFileImport = async (data: ImportForm) => {
     try {
       const text = await getFileText(data[ImportFormNames.FileList][0]);
       console.log(text);
       // try to parse a file
       // throw error in case of failure
+      const parsedCards = await parseCards(text);
       // get current cards from app state
       // compare them based on "code" and "formatCode". Add unique records
+      const uniqParsedCards = getUniqParsedCards(parsedCards, state.cards);
+      console.log(uniqParsedCards);
       // display count of added records in dialog
       // add options to navigate to my cards and to import more data
     } catch (e) {
