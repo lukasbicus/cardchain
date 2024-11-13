dd359d14a0bb0b53211afed296b22eff70b9e3ea
lukasbicus <ing.lukas.bicus@gmail.com>
2024-11-13 09:49:31 +0100

    Implement mark favorite

--- app/(homescreens)/my-cards/my-cards.tsx
+++ app/(homescreens)/my-cards/my-cards.tsx
@@ -1,17 +1,18 @@
 'use client';
 
 import useAppState from '@/app/lib/app-state/app-state';
+import { AppActionTypes } from '@/app/lib/app-state/reducer';
 import { filterByQuery } from '@/app/lib/filterByQuery';
 import { Routes } from '@/app/lib/shared';
-import { IconStar } from '@tabler/icons-react';
 import { CompanyIcon } from '@/app/ui/company-icon';
+import { IconStar, IconStarFilled } from '@tabler/icons-react';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 
 export default function MyCards() {
   const searchParams = useSearchParams();
   const query = searchParams.get('query')?.toString();
-  const [state] = useAppState();
+  const [state, dispatch] = useAppState();
 
   return (
     <ul className="menu menu-sm rounded-box gap-2">
@@ -35,8 +36,23 @@ export default function MyCards() {
               <CompanyIcon {...card} />
             </span>
             <span className="text-xl">{card.name}</span>
-            <button className="btn btn-ghost btn-square btn-primary">
-              <IconStar className="h-6 w-6" />
+            <button
+              className="btn btn-ghost btn-circle btn-primary"
+              onClick={e => {
+                e.preventDefault();
+                dispatch({
+                  type: AppActionTypes.ToggleCardFavorite,
+                  payload: {
+                    id: card.id,
+                  },
+                });
+              }}
+            >
+              {card.favorite ? (
+                <IconStarFilled className="h-6 w-6" />
+              ) : (
+                <IconStar className="h-6 w-6" />
+              )}
             </button>
           </Link>
         </li>
