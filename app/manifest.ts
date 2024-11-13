5dba12b1873a417358a1d9a39917635fb3c57dc7
lukasbicus <ing.lukas.bicus@gmail.com>
2024-11-13 13:05:47 +0100

    Update manifest (#20)
    * Update manifest
    
    * Update manifest.

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,4 +1,4 @@
-import { ICON_COLOR } from '@/app/lib/shared';
+import { PRIMARY_COLOR } from '@/app/lib/shared';
 import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
@@ -8,8 +8,8 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'A loyalty card app',
     start_url: '/',
     display: 'standalone',
-    theme_color: ICON_COLOR,
-    background_color: '#fff',
+    theme_color: PRIMARY_COLOR,
+    background_color: PRIMARY_COLOR,
     orientation: 'portrait',
     icons: [
       {
